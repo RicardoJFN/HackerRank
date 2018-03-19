05b8fb190870a6a6963771dafcd7fd6694981461
Ricardo Nunes <ricardonunes@trivalor.pt>
2018-03-19 21:04:36 +0000

    First test case is Ok but it fails the following tests.

--- Challenges/src/Algorithms/DiagonalDifference.java
+++ Challenges/src/Algorithms/DiagonalDifference.java
@@ -4,14 +4,16 @@ public class DiagonalDifference {
 
     public static int diagonalDifference(int[][] arr){
 
+
+        //Continuar mas fazer uma matriz com int n como parametro do método.
         int difference = 0;
 
         int a0 = arr[0][0], a1 = arr[1][1], a2 = arr[2][2];
-        int b2 = arr[0][2], b1 = arr[1][1], b0 = arr[0][0];
+        int b2 = arr[0][2], b1 = arr[1][1], b0 = arr[2][0];
 
         int totalA = a0 + a1 + a2, totalB = b2 + b1 + b0;
 
-        return difference = (totalA - totalB);
+        return Math.abs(difference = (totalA - totalB));
 
     }
 
--- Challenges/src/MainTester.java
+++ Challenges/src/MainTester.java
@@ -10,23 +10,18 @@ public class MainTester {
 
         int[] simpleArraySumNumbers = new int[] { 1, 2, 3, 4, 10, 11 };
         long[] longArraySumNumbers = new long[] {1000000001, 1000000002, 1000000003, 1000000004, 1000000005};
-        /*int[][] diagonalDiffArray = new int[3][3];
-        System.out.println(diagonalDiffArray.length);
-        Arrays.fill(diagonalDiffArray[0], 11);
-        Arrays.fill(diagonalDiffArray[1], 4);
-        Arrays.fill(diagonalDiffArray[2], 10);
-        Arrays.fill(diagonalDiffArray[3], 2);
-        Arrays.fill(diagonalDiffArray[4], 5);
-        Arrays.fill(diagonalDiffArray[5], 6);
-        Arrays.fill(diagonalDiffArray[6], 10);
-        Arrays.fill(diagonalDiffArray[7], 8);
-        Arrays.fill(diagonalDiffArray[8], -12);
+        int[][] diagonalDiffArray = new int[3][3];
 
-        System.out.println("--------------------------------\n" +
-                            "Array filled: ");
-        for(int i = 0; i != diagonalDiffArray.length; i++){
-            System.out.println(diagonalDiffArray[i][i]);
-        }*/
+        diagonalDiffArray[0][0] =  11;
+        diagonalDiffArray[0][1] = 2;
+        diagonalDiffArray[0][2] = 4;
+        diagonalDiffArray[1][0] = 4;
+        diagonalDiffArray[1][1] = 5;
+        diagonalDiffArray[1][2] = 6;
+        diagonalDiffArray[2][0] = 10;
+        diagonalDiffArray[2][1] = 8;
+        diagonalDiffArray[2][2] =  -12;
+
 
 
         System.out.println(TextClass.SOLVE_ME_FIRST + SolveMeFirst.solve(3, 4));
@@ -34,7 +29,7 @@ public class MainTester {
         System.out.println(TextClass.COMPARE_TRIPLETS_ALICE + CompareTriplets.solve(5, 6, 7, 3, 6, 10)[0]);
         System.out.println(TextClass.COMPARE_TRIPLETS_BOB + CompareTriplets.solve(5, 6, 7, 3, 6, 10)[1]);
         System.out.println(TextClass.A_VERY_BIG_SUM + BigSum.aVeryBigSum(5, longArraySumNumbers));
-//        System.out.println(TextClass.DIAGONAL_DIFFERENCE + DiagonalDifference.diagonalDifference(diagonalDiffArray));
+        System.out.println(TextClass.DIAGONAL_DIFFERENCE + DiagonalDifference.diagonalDifference(diagonalDiffArray));
     }
 
 }
